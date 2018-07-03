8f6c9975fc4b367e1591da31495676ef23f7efac
jfrazier <johnfrazier1222@gmail.com>
2018-07-03 15:06:36 -0400

    learned javascript loops to dynamically create images

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('it works');
+var funko = ['camericacopy.png','captaincopy.jpg','flashcopy.png','lobocopy.jpg','rnwcopy.png'];
+
+var gallery = document.getElementById('gallery');
+
+// var count= 0;
+// for (thisItem in funko) {
+//  console.log(funko[count]);
+//  count = count + 1;
+// };
+
+var count = 0;
+for (thisItem in funko) {
+  var newImage = document.createElement('img');
+  newImage.src = 'img/' + funko[count];
+  newImage.width = '150';
+  newImage.height = '100';
+  gallery.appendChild(newImage);
+  count = count + 1;
+};
